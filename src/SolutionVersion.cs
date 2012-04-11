e8f461d40a483c45ffed68d56bb946daa804d9eb
Chris Patterson <chris@phatboyg.com>
2012-04-10 20:31:03 -0500

    Added SolutionVersion.cs for those VS users

---
+++ src/SolutionVersion.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyDescription("Odoyule Rules, a .NET rules engine")]
+[assembly: AssemblyProduct("OdoyuleRules")]
+[assembly: AssemblyCopyright("Copyright 2011-2012 Chris Patterson, All rights reserved.")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.12101")]
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+
